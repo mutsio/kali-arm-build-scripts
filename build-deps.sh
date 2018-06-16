260e88a847a4da27eae116e9121b87746b58a2f5
mutsio <40067008+mutsio@users.noreply.github.com>
2018-06-16 17:05:55 -0400

    Cleanup

--- build-deps.sh
+++ build-deps.sh
@@ -5,7 +5,9 @@ zip curl libncurses5-dev zlib1g-dev libncurses5-dev gcc-multilib g++-multilib \
 parted kpartx debootstrap pixz qemu-user-static abootimg cgpt vboot-kernel-utils \
 vboot-utils u-boot-tools bc lzma lzop automake autoconf m4 dosfstools rsync \
 schedtool git dosfstools e2fsprogs device-tree-compiler libssl-dev qemu-user-static \
-btrfs-tools crossbuild-essential-armhf
+btrfs-tools 
+
+apt-get install -y crossbuild-essential-armhf
 
 MACHINE_TYPE=`uname -m`
 if [ ${MACHINE_TYPE} == 'x86_64' ]; then
